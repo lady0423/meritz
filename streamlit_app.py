80deb3ed6f155d34abf2a44cf3b21a925b653640
lady0423 <pjy840423@gmail.com>
2026-03-21 17:55:50 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,5 @@ import streamlit as st
 st.set_page_config(page_title="실적 안내장 조회", layout="wide")
 
 st.title("📊 실적 안내장 조회")
-st.write("테스트 화면입니다.")
+st.write("## 테스트 화면")
+st.write("이 메시지가 보이면 기본 설정은 성공입니다! ✅")
